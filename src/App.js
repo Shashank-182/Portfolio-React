a65e7589cb5b42489a4a83e9c12bd123618b1bd9
Shashank-182 <shashankhanumandla@gmail.com>
2024-09-01 22:41:11 +0530

    Fixed CSS import issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import NavigationBar from './Components/NavigationBar';
 import Home from './Components/Home';
 import About from './Components/About';
